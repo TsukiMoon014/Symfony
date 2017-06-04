3bf08feffe5f4bf6d704f43b826f1f555a6dcbe2
Unknown <tsukimoon014@gmail.com>
2017-06-04 18:29:54 +0200

    Repositories, repositories

--- src/OC/PlatformBundle/Repository/AdvertRepository.php
+++ src/OC/PlatformBundle/Repository/AdvertRepository.php
@@ -10,4 +10,57 @@ namespace OC\PlatformBundle\Repository;
  */
 class AdvertRepository extends \Doctrine\ORM\EntityRepository
 {
+	public function myFindAll()
+	{
+		return $this
+		->createQueryBuilder('a')
+		->getQuery()
+		->getResult();
+	}
+
+	public function myFind($id){
+		return $this
+		->createQueryBuilder('a')
+		->where('a.id = :id')
+		->setParameter(":id",$id)
+		->getQuery()
+		->getResult();
+	}
+
+	public function findByAuthorAndDate($author,$date){
+		$db = $this->createQueryBuilder('a')
+		->where('a.author = :author')
+		->setParameter(":author",$author);
+
+		$this->whereCurrentYear($db);
+
+		$db->orderBy("a.date","DESC");
+
+		return $db->getQuery()->getResult();
+	}
+
+	public function whereCurrentYear(QueryBuilder $qb){
+		$qb->andWhere("a.date BETWEEN :start AND :end")
+		->setParameter(":start", new \DateTime(date('Y').'-01-01'))
+		->setParameter(":end", new \DateTime(date('Y').'-12-31'));
+	}
+
+	public function getAllAdvertWithApplications(){
+		return $this
+		->createQueryBuilder('a')
+		->leftJoin('a.applications','app')
+		->addSelect('app')
+		->getQuery()
+		->getResult();
+	}
+
+	public function getAllAdvertWithCategories(array $listCategories){
+		return $this
+		->createQueryBuilder('a')
+		->leftJoin('a.categories','c')
+		->addSelect('c')
+		->where($qb->expr()->in('c.name', $listCategories))
+		->getQuery()
+		->getResult();
+	}
 }
